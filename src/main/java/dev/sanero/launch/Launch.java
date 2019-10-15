1f8e0ae72ab4e08173fa8f2c0a91aad76597b368
AMS6 <tgn296@gmail.com>
2019-10-15 16:51:07 +0700

    except equals

--- src/main/java/dev/sanero/launch/Launch.java
+++ src/main/java/dev/sanero/launch/Launch.java
@@ -1,17 +1,18 @@
 package dev.sanero.launch;
 
+import java.io.File;
+import java.util.List;
+
+import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
+
 import dev.sanero.module.ExcelReader;
 import dev.sanero.module.FileManagement;
 import dev.sanero.module.ScreenInformation;
 import dev.sanero.util.StringUtils;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.List;
-
 public class Launch {
-    private final static String MAPPER_APP_PATH_DIR = "H:\\source checkout\\No03-Source Code\\trunk\\ONTICERP_JW-Pharma\\src\\main\\resources\\mapper\\application\\";
-    private final static String MAPPER_COMMON_PATH_DIR = "H:\\source checkout\\No03-Source Code\\trunk\\ONTICERP_JW-Pharma\\src\\main\\resources\\mapper\\common\\";
+    private final static String MAPPER_APP_PATH_DIR = "D:\\ontic_pj_documents\\fromSVN\\No03-Source Code\\trunk\\ONTICERP_JW-Pharma\\src\\main\\resources\\mapper\\application\\";
+    private final static String MAPPER_COMMON_PATH_DIR = "D:\\ontic_pj_documents\\fromSVN\\No03-Source Code\\trunk\\ONTICERP_JW-Pharma\\src\\main\\resources\\mapper\\common\\";
 
     public static void main(String[] args) {
         List<ScreenInformation> screenInfoList = ExcelReader.getListScreenInformation();
@@ -32,7 +33,9 @@ public class Launch {
                 System.out.println(screenInfo.getKorName() + " is not have mapper file");
             } else {
                 StringBuilder builder = FileManagement.readMethodInFile(filePath, "selectTbLeLotList");
-                System.out.println(builder);
+                String formatted = new BasicFormatterImpl().format(builder.toString());
+//                System.out.println(builder.toString().replaceAll("[\n]+", "\n").replaceAll("\n[ \t]*OR", " OR"));
+                System.out.println(formatted);
             }
 //        }
     }
--- src/main/java/dev/sanero/module/FileManagement.java
+++ src/main/java/dev/sanero/module/FileManagement.java
@@ -1,15 +1,25 @@
 package dev.sanero.module;
 
-import dev.sanero.util.StringUtils;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Supplier;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+import dev.sanero.util.StringUtils;
 
 public class FileManagement {
     public static StringBuilder readFile(String filePath) {
@@ -24,12 +34,26 @@ public class FileManagement {
 
     public static StringBuilder readMethodInFile(String filePath, String method) {
         StringBuilder builder = new StringBuilder();
-        try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+        try {
+        	Supplier<Stream<String>> supplier = () -> {
+				try {
+					return Files.lines(Paths.get(filePath));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+					return null;
+				}
+			};
             AtomicBoolean isCorrectMethod = new AtomicBoolean(false);
             AtomicReference<String> methodKey = new AtomicReference<>("");
             HashMap<String, String> mapParam = new HashMap<>();
             AtomicInteger currentIndex = new AtomicInteger(1);
+            final String[] tags = {""};	// tags[0] = "<choose><when><choose>....."
+            AtomicBoolean inBlock = new AtomicBoolean(false);
+            AtomicBoolean isFirst = new AtomicBoolean(true);
+            Set<String> params = Collections.synchronizedSet(new HashSet<>());
 
+            Stream<String> stream = supplier.get();
             stream.forEach(line -> {
                 if (isCorrectMethod.get()) {
                     if (!StringUtils.isNullOrEmpty(methodKey.get()) && line.contains("/" + methodKey.get())) {
@@ -39,6 +63,43 @@ public class FileManagement {
                         if (line.contains("/*")) {
                             line = line.replace("/*", "--").replace("*/", "");
                         }
+                        if (line.contains("<where>")) {
+                        	line = line.replace("<where>", "WHERE 1=1");
+                        }
+                        if (line.contains("</where>")) {
+                        	line = line.replace("</where>", "");
+                        }
+                        if (line.contains("<![CDATA[")) {
+                        	line = line.replace("<![CDATA[", "");
+                        	line = line.replace("]]>", "");
+                        }
+                        if(line.contains("if") || line.contains("choose") || line.contains("when") || line.contains("otherwise")) {
+                        	if(line.contains("/")) {
+                        		tags[0] = tags[0].substring(0, tags[0].length() - line.trim().length() + 1);
+                        		if(tags[0].length() == 0) {
+                        			inBlock.set(false);
+                        			StringBuilder sb = new StringBuilder();
+									params.stream().forEach(s -> sb.append(" AND " + mapParam.get("#{" + s + "}") + " IS NULL "));
+                        			sb.append("))");
+                        			line = sb.toString();
+                        			line = line.replaceFirst("AND", "OR(");
+                        			params.clear();
+                        		}
+                        	} else {
+                        		if(tags[0].length() == 0) {
+                        			isFirst.set(true);
+                        		}
+                        		tags[0] += (line.trim().substring(0, line.trim().indexOf(' ') != -1 ? line.trim().indexOf(' ') : line.trim().indexOf('>'))+">");
+                        		inBlock.set(true);
+                        		if(line.contains("test")) {
+                        			params.addAll(extractParam(line));
+                        		}
+                        		
+                        	}
+                        }
+                        if(line.matches(".*if.*") || line.matches(".*choose.*") || line.matches(".*when.*") || line.matches(".*otherwise.*")) {
+                        	line = line.substring(line.indexOf(">")+1);
+                        }
                         if (line.contains("#{")) {
                             while(line.contains("#{")){
                                 String param = line.substring(line.indexOf("#{"), line.indexOf("}")+1);
@@ -46,6 +107,23 @@ public class FileManagement {
                                     mapParam.put(param, "V_ARG_" + currentIndex.getAndIncrement());
                                 }
                                 line = line.replace(param,  mapParam.get(param));
+                            }
+                            if(line.matches(".*[ \t]+AND[ \t]+.*")){
+                            	Matcher matcher = Pattern.compile("[ \t]AND[ \t]+").matcher(line);
+                            	if(matcher.find()) {
+                                	int indexOfAND = matcher.start()+1;
+                                	if(tags[0].length() > 0) {
+                                		if(inBlock.get()) {
+                                			if(isFirst.get()) {
+                                				line = line.substring(0, indexOfAND) + "AND(" + line.substring(indexOfAND + 3);
+                                				isFirst.set(false);
+                                			} else {
+                                				line = line.substring(0, indexOfAND) + "OR" + line.substring(indexOfAND + 3);
+                                			}
+                                			
+                                		}
+									}
+								}
                             }
                         }
                         builder.append(line).append("\n");
@@ -57,10 +135,56 @@ public class FileManagement {
                     isCorrectMethod.set(true);
                 }
             });
+            
+//            stream = supplier.get();
+//            stream.forEach(line -> {
+//                if (isCorrectMethod.get()) {
+//                    if (!StringUtils.isNullOrEmpty(methodKey.get()) && line.contains("/" + methodKey.get())) {
+//                        isCorrectMethod.set(false);
+//                        methodKey.set("");
+//                    } else {
+//                        convertCondition(line, mapParam);
+//                    }
+//                }
+//
+//                if (line.contains("\"" + method + "\"") && !line.contains("/*")) {
+//                    methodKey.set(line.trim().substring(1, line.indexOf(" ") - 1));
+//                    isCorrectMethod.set(true);
+//                }
+//            });
 
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return builder;
+    }
+    
+    public static String convertCondition(String line, HashMap<String, String> mapParam) {
+    	String pattern1 = ".*";
+    	String pattern2 = ".*";
+    	String initialParamName = "";
+    	for(String initialParamExpression : mapParam.keySet()) {
+    		initialParamName = initialParamExpression.substring(2, initialParamExpression.length() - 1);
+    		pattern1 = ".*" + initialParamName + "[ ]+!=[ ]+null.*";
+    		pattern2 = ".*" + initialParamName + "[ ]+!=[ ]+''.*";
+    		if(line.matches(pattern1) && line.matches(pattern2)) {
+    			System.out.println(line);
+    		}
+    	}
+    	return "";
+    }
+    
+    public static Set<String> extractParam(String line) {
+    	String insideTest = line.substring(line.indexOf('\"')+1, line.lastIndexOf('\"'));
+    	String tmp = insideTest.replaceAll("null", " ");
+    	tmp = tmp.replaceAll("!=", " ");
+    	tmp = tmp.replaceAll("''", " ");
+    	tmp = tmp.replaceAll("[ ]+", " ");
+    	tmp = tmp.replaceAll("[\t]+", " ");
+    	tmp = tmp.replaceAll("and", " ");
+    	tmp = tmp.replaceAll("or", " ");
+    	String[] params = tmp.split(" ");
+    	List<String> duplicated = Arrays.asList(params);
+    	return duplicated.stream().filter(s -> s.trim().length() > 0).collect(Collectors.toSet());
     }
 }
